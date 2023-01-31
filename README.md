24ab58970b182eea33444ebaa3fb1dc6edc244e6
Giftina <admin@giftia.moe>
2023-01-31 19:01:14 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # original_mipi_init_sequence-to-rk_dts_panel_init_sequence-convert
  用于将厂家给的mipi屏初始化代码转换为RK系列的dts文件的panel-init-sequence。
+
+# 效果
+例：将DTS数据:
+```
+SSD_CMD(0x30);
+SSD_PAR(0x01);
+
+SSD_CMD(0x78);
+SSD_PAR(0x49);
+SSD_PAR(0x61);
+SSD_PAR(0x02);
+SSD_PAR(0x00);
+
+SSD_CMD(0x29);
+Delayms(50);
+```
+
+转换为RK数据:
+```
+15 00 30 01 01
+39 00 78 05 49 61 02 00
+05 32 01 29
+```
+
+数据格式参考 https://blog.csdn.net/qq_38312843/article/details/107534743
+
+# 用法
+  1. 在 [releases](https://github.com/Giftia/Lapelo/releases) 获取最新的压缩包，下载到本地并解压。
+  2. 将厂家给的mipi屏初始化代码复制到 `mipi_init_sequence.txt` 文件中。
+  3. 运行 `original_mipi_init_sequence-to-rk_dts_panel_init_sequence-convert.exe` 。
+  4. 将生成的 `panel-init-sequence.txt` 里的数据复制到你屏幕dts文件中的 `panel-init-sequence` 。
---
+++ index.js
@@ -0,0 +1,66 @@
+"use strict";
+/**
+ * Author: Giftina: https://github.com/Giftia/
+ * 用于将厂家给的mipi屏初始化代码转换为RK系列的dts文件的panel-init-sequence。
+ * 本程序基于 MIT 协议开源，欢迎 Fork 和 Star
+ * Version: 0.0.1
+ */
+
+// 数据格式参考 https://blog.csdn.net/qq_38312843/article/details/107534743
+
+const fs = require('fs');
+
+function main() {
+  const file = fs.readFileSync('mipi_init_sequence.txt', 'utf8');
+  const lines = file.split('\r\n');
+  const CMD_Type = { delay: '05', single: '15', multi: '39' };
+  const lineSample = '{ "Type": "", "Delay": 0, "DataCount": 1, "Data": [] }'; // 命令类型，延时，数据长度，数据
+  let lineTemp = { "Type": "", "Delay": 0, "DataCount": 1, "Data": [] };
+  const result = []; // lineSample[]
+  let CMDInProcess = false;
+
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i];
+    if (line.startsWith('SSD_CMD') || i === lines.length - 1) { // 遇到新的CMD或者到达文件末尾
+      if (CMDInProcess) {
+        // 上一组CMD已经结束，要将数据写入result
+        CMDInProcess = false;
+        // 判断CMD类型
+        if (lineTemp.Type !== CMD_Type.delay) {
+          if (lineTemp.DataCount > 2) {
+            lineTemp.Type = CMD_Type.multi;
+          } else {
+            lineTemp.Type = CMD_Type.single;
+          }
+        }
+        // 将count转换为16进制
+        lineTemp.DataCount = lineTemp.DataCount.toString(16).padStart(2, '0');
+        // 将delay转换为16进制
+        lineTemp.Delay = lineTemp.Delay.toString(16).padStart(2, '0');
+        // 将lineTemp写入result
+        result.push(`[${lineTemp.Type} ${lineTemp.Delay} ${lineTemp.DataCount} ${lineTemp.Data.join(' ')}]`);
+        // 重置lineTemp
+        lineTemp = JSON.parse(lineSample);
+      }
+      CMDInProcess = true;
+      if (i !== lines.length - 1) {
+        lineTemp.Data.push(line.split('x')[1].split(')')[0]); // 将CMD写入Data
+      }
+    }
+    else if (CMDInProcess && line.startsWith('SSD_PAR')) {
+      lineTemp.DataCount++;
+      lineTemp.Data.push(line.split('x')[1].split(')')[0]); // 将PAR写入Data
+    }
+    else if (CMDInProcess && line.startsWith('Delayms')) {
+      lineTemp.DataCount++;
+      lineTemp.Delay = Number(line.split('(')[1].split(')')[0]); // 将Delayms写入Delay
+      lineTemp.Type = CMD_Type.delay;
+    }
+  }
+
+  const resultStr = result.map(item => item.slice(1, -1)).join('\r\n');
+  console.log(resultStr);
+  fs.writeFileSync('panel-init-sequence.txt', resultStr);
+}
+
+main();
